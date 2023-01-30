894c053314f3e6af99bc34ab1d238c33780ac151
phat-612 <phatgowin123@gmail.com>
2023-01-30 22:09:04 +0700

    upadte
    add time, scores

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 // component
-const btnShowMean = document.getElementById("btn_show_mean");
+const btnShowEn = document.getElementById("btn_show_en");
+const btnShowVi = document.getElementById("btn_show_vi");
 const btnShowPro = document.getElementById("btn_show_pro");
+const time = document.getElementById("time");
+const scores = document.getElementById("scores");
 const word_en = document.getElementById("word_en");
 const word_vi = document.getElementById("word_vi");
 const word_pro = document.getElementById("word_pro");
 const input_text = document.getElementById("input_text");
+
+var totalScores = 0;
+var totalTime = 0;
+var first = true;
 // function
 
 function check_input() {
+  if (first) {
+    startTime();
+    first = false;
+  }
   let temp_word = word_en.innerText;
   let temp_input = input_text.value;
   if (temp_input == temp_word) {
     input_text.value = "";
     show_word();
+    addScoers();
   }
 }
 
@@ -25,9 +37,32 @@ function hideElement(ele) {
   }
 }
 
+function addScoers() {
+  totalScores += 1;
+  scores.innerText = "Scores: " + totalScores;
+}
+
+function startTime() {
+  setInterval(() => {
+    let minute = Math.floor(totalTime / 60);
+    if (minute < 10) {
+      minute = "0" + minute;
+    }
+    let second = totalTime % 60;
+    if (second < 10) {
+      second = "0" + second;
+    }
+    time.innerText = "Time: " + minute + ":" + second;
+    totalTime += 1;
+  }, 1000);
+}
+
 //main
 input_text.addEventListener("input", check_input);
-btnShowMean.addEventListener("click", () => {
+btnShowEn.addEventListener("click", () => {
+  hideElement(word_en);
+});
+btnShowVi.addEventListener("click", () => {
   hideElement(word_vi);
 });
 btnShowPro.addEventListener("click", () => {
